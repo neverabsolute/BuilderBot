4781b88cfdf981d0a56248c5e0b54720d7484068
Jackson <jacksonchoyce@gmail.com>
2024-08-21 21:42:36 -0500

    Update send.ts

--- apps/bot/src/events/discord/message/send.ts
+++ apps/bot/src/events/discord/message/send.ts
@@ -28,45 +28,5 @@ export class HandleMessageCreate {
 				await message.delete().catch(() => {});
 			}
 		}
-
-		const markdownLinkRegex = /\[([^\]]+)\]\((https?:\/\/[^\s]+)\)/g;
-		let match;
-
-		while ((match = markdownLinkRegex.exec(message.content)) !== null) {
-			const linkText = match[1];
-			const actualLink = match[2];
-
-			const url = new URL(actualLink);
-
-			if (url.hostname.includes(linkText)) {
-				return;
-			}
-
-			if (url.hostname === "cdn.discordapp.com") {
-				return;
-			}
-
-			const discordian = message.member;
-
-			if (!discordian || !(discordian instanceof GuildMember)) {
-				return;
-			}
-
-			if (
-				discordian.permissions.has("ManageGuild") ||
-				discordian.permissions.has("Administrator") ||
-				discordian.permissions.has("ManageMessages")
-			) {
-				return;
-			}
-
-			await message.delete().catch(() => {});
-
-			await discordian
-				.timeout(5 * 60 * 1000, "Attempting to mask a link as another link")
-				.catch(() => {});
-
-			break;
-		}
 	}
 }
