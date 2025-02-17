c48d559f85f9963b88c9e212e2c88715fa355faa
Jackson <jacksonchoyce@gmail.com>
2025-02-17 02:44:51 -0600

    sheepy gif deleter 9000

--- apps/bot/src/events/discord/message/send.ts
+++ apps/bot/src/events/discord/message/send.ts
@@ -2,7 +2,7 @@ import { GuildMember } from "discord.js";
 import type { ArgsOf } from "discordx";
 import { Discord, On } from "discordx";
 import { FOOTPRINTS_CHANNEL } from "../../../configs.js";
-
+import { prisma } from "bot-prisma";
 @Discord()
 export class HandleMessageCreate {
 	@On()
@@ -27,6 +27,26 @@ export class HandleMessageCreate {
 			if (!message.attachments.size && !message.embeds.length) {
 				await message.delete().catch(() => {});
 			}
+		}
+
+		if (!(message.author.id === "1278775823704391823")) return;
+
+		const hasLinkInMessage = message.content.match(/(https?:\/\/[^\s]+)/g);
+
+		if (!hasLinkInMessage) return;
+
+		const messages = await prisma.message.findMany({
+			where: {
+				userId: BigInt(message.author.id),
+				createdAt: {
+					gte: new Date(Date.now() - 1000 * 60 * 60)
+				}
+			}
+		});
+
+		const linkCount = messages.filter((m) => m.content.match(/(https?:\/\/[^\s]+)/g)).length;
+		if (linkCount > 3) {
+			await message.delete().catch(() => {});
 		}
 	}
 }
